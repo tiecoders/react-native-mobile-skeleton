a4774191559b45d0449013206a99be8aa2cd206e
TieCoders <a.hannachi@tiecoders.com>
2019-11-12 22:14:19 +0100

    'fixes'

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,13 @@
 export {
   textStyle,
 } from './style';
+
+export {
+  ValidationInput,
+  ValidationInputProps,
+} from './validationInput.component';
+
+export {
+  ScrollableAvoidKeyboard,
+  ScrollableAvoidKeyboardProps,
+} from './scrollableAvoidKeyboard.component';
--- src/components/screens/HomeScreen/HomeScreen.js
+++ src/components/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Layout, Text } from 'react-native-ui-kitten';
+import {
+    View,
+    ImageBackground
+} from 'react-native';
+import {
+    Button,
+    Text
+} from 'react-native-ui-kitten'
+import {textStyle, ScrollableAvoidKeyboard} from "../../common";
 import styles from './styles'
 
 class HomeScreen extends React.Component{
 
     render = () => (
-        <Layout>
-            <Text>This is the home</Text>
-        </Layout>
+        <></>
     )
 }
 
